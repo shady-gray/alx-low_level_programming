e617924a8fc27358405894cbf2422d7afef0118b
Basil Bassey <akpanbasilbassey@gmail.com>
2022-06-30 22:34:26 -0700

    hopes this works

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* main - Entry point */
+/**
+ * main - Entry point 
+ *
+ *  Return: 0 number
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
