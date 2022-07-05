69633cb50149b79e91b00260ed95640f833585fd
Basil Bassey <akpanbasilbassey@gmail.com>
2022-07-05 10:26:22 -0700

    2-print_alphabet_x10.c file added to directory

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet -> prints the lowercase alphabets x10
+*/
+
+void print_alphabet_x10(void)
+{
+	int j, i;
+	i=0;
+	while (i < 10)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+	_putchar('\n');
+	i++;
+	}
+}
