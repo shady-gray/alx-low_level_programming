96a024a29350bf068e07366f0dbb7d4dab38332e
Basil Bassey <akpanbasilbassey@gmail.com>
2022-07-07 08:47:44 -0700

    Added 0-main.c file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check if number is positve or negative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	i = 34;
+
+	positive_or_negative(i);
+return (0);
+}
