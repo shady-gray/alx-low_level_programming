7b2e0e989cf90b493a20a7cc04f7151a4e08c1cb
Basil Bassey <akpanbasilbassey@gmail.com>
2022-07-08 15:52:13 -0700

    added files

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,9 @@
-int _isdigit(int c);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
@@ -8,3 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
+
+#endif /* MAIN_H */
