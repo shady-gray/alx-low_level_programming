0ab0cc2bb5af676e1335b69e2283f0c54fb8196a
Basil Bassey <akpanbasilbassey@gmail.com>
2022-07-18 21:26:15 -0700

    made corrections to 0-memset.c and 1-memcpy.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,13 +9,13 @@
  *
  * Return: A pointer to the filled memory area @s.
  */
-void *_memset(void *s, int c, size_t n)
+void *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+unsigned int index;
+unsigned char *memory = s, value = c;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
+for (index = 0; index < n; index++)
+memory[index] = value;
 
-	return (memory);
+return (memory);
 }
