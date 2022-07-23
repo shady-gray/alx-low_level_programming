06f0d9ed4c9ad7d4536a3b2a951959de05c93b24
Basil Bassey <akpanbasilbassey@gmail.com>
2022-07-22 22:06:29 -0700

    added all task files

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,13 @@
-#include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - prints the number of arguments passed into it
- * @argc: the argument count
- * @argv: the arrau the argument is pointing to
- * Return: 0
+ * main - prints the number of arguments passed into it.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argv[argc - 1]);
+	printf("%d\n", argc - 1);
 	return (0);
 }
