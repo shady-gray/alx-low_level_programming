ea05a95e5d622712f72e76716c3a692bc7e5a058
Basil Bassey <akpanbasilbassey@gmail.com>
2023-04-05 13:43:18 -0700

    Adding Task 5 file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *
+ * free_listint2 - a function that frees a lisint_t list
+ * @head: Double pointer to list
+ *
+ * Return: void.
+ * AUTHOR: BASIL BASSEY
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *next_node;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		next_node = (*head)->next;
+		free(*head);
+		*head = next_node;
+	}
+}
+
