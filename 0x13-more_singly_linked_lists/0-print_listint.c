aa26bf271c0c7f22d7c9f35fd8c1cea80ae1ee0b
Basil Bassey <akpanbasilbassey@gmail.com>
2023-04-05 08:54:39 -0700

    Making changes to header file and taask 0 file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 /**
  * print_listint - prints all the elements 
  * of a listint_t list.
  * @h: head of node
  *
- * Return: number of nodes.
- * @AUTHOR: BASIL BASSEY
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef _LISTS_H
 #define _LISTS_H
 
+#include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -32,4 +34,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif /* _LISTS_H */
+#endif /*_LISTS_H*/
