8cd1657ddb37c9585dade09f8c7eb5494380d83a
Basil Bassey <akpanbasilbassey@gmail.com>
2023-04-05 09:44:17 -0700

    Adding task 2 file

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * add_nodeint - add node at beginning of a listint_t list.
+  * @head: head of double pointer
+  * @n: int add the list
+  *
+  * AUTHOR: BASIL BASSEY
+  * Return: NULL if it failed,
+  * returns address of the new element, On Success.
+  */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *ptr;
+
+	if (head == NULL)
+		return (NULL);
+
+	ptr = malloc(sizeof(listint_t));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr->n = n;
+	ptr->next = *head;
+	*head = ptr;
+
+	return (ptr);
+}
+
