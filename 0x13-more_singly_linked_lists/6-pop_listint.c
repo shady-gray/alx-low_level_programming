2149362d3531c9c686c336aca29ecf65e9aa96e8
Basil Bassey <akpanbasilbassey@gmail.com>
2023-04-05 14:09:21 -0700

    Adding content to .gitignore file and also upadating a correction on task 6 file

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,23 +1,25 @@
 #include "lists.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * pop_listint - a function that frees a listint_t list
  * @head: double pointer to the list.
  *
- * Return: 0 if linked list is empty, n if not empty.
+ * Return: 0 if linked list is empty, i if not empty.
  * AUTHOR: BASIL BASSEY
  */
 int pop_listint(listint_t **head)
 {
 	listint_t *begin_node;
-	int n;
+	int i;
+
 	if (head == NULL || *head == NULL)
 		return (0);
+
 	begin_node = *head;
 	*head = begin_node->next;
-	n = begin_node->n;
+	i = begin_node->n;
 	free(begin_node);
-	return (n);
+	return (i);
 }
 
