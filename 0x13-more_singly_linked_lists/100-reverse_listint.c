caa5ae368678de234a0be01a8b6439c6e62e4cbf
Basil Bassey <akpanbasilbassey@gmail.com>
2023-04-05 14:51:26 -0700

    Adding task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - a function that reverses a listint_t
+ * @head: double pointer to head
+ * 
+ * Return: a pointer to the first node of the reversed list
+ * AUTHOR: BASIL BASSEY
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *previous_node, *next_node;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	if ((*head)->next == NULL)
+		return (*head);
+
+	previous_node = NULL;
+
+	while (*head != NULL)
+	{
+		next_node = (*head)->next;
+		(*head)->next = previous_node;
+		previous_node = *head;
+		*head = next_node;
+	}
+
+	*head = previous_node;
+
+	return (*head);
+}
+
