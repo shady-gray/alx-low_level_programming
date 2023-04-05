184a7167fae3dc47d59cdf6f23416dae50d31c74
Basil Bassey <akpanbasilbassey@gmail.com>
2023-04-05 14:20:55 -0700

    Adding Task 8 file

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * data_sum_listint - a function that returns the
+ * sum of all the data (n) of a listint_t linked list.
+ * @head: pointer to the first node
+ *
+ * Return: 0, if list is empty. data_sum of linked_t linked list if not empty
+ * AUTHOR: BASIL BASSEY
+ */
+int data_sum_listint(listint_t *head)
+{
+	int s = 0;
+
+	while (head != NULL)
+	{
+		s += head->n;
+		head = head->next;
+	}
+
+	return (s);
+}
+
