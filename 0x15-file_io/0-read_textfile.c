4fedfb7f7da8e44b8ff1130ae8882bbfebb76a0c
Basil Bassey <akpanbasilbassey@gmail.com>
2023-04-09 13:13:49 -0700

    Making corrections

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,36 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * read_textfiles - a function that reads a text file and prints it to the
+ * read_textfile - a function that reads a text file and prints it to the
  *                  POSIX standard output.
  * @filename: file pathname.
  * @letters: number of letters to read or print
- * 
+ *
  * Return: actual number of letter it could read and print
  * AUTHOR: BASIL BASSEY
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, wr, rd;
-    char *buff;
-    if (filename == NULL)
-        return (0);
-    
-    buff = malloc(sizeof(char) * letters);
-    if (buff == NULL)
-        return (0);
+	ssize_t file, wr, rd;
+	char *buff;
 
-    file = open(filename, O_RDONLY);
-    rd = read(file, buff, letters);
-    wr = write(STDOUT_FILENO, buff, file);
-    if (file == -1 || wr == -1 || rd == -1)
-    {
-        free(buff);
-        return (0);
-    }
+	if (filename == NULL)
+		return (0);
 
-    free(buff);
-    return wr;
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	rd = read(file, buff, letters);
+	wr = write(STDOUT_FILENO, buff, file);
+	if (file == -1 || wr == -1 || rd == -1)
+	{
+		free(buff);
+		return (0);
+	}
+
+	free(buff);
+	return (wr);
 }
 
