6fb2a4297c842723b69e5b7b7cab69ee0a03f2f2
Basil Bassey <akpanbasilbassey@gmail.com>
2023-04-09 14:17:06 -0700

    Making corrections to Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  * @text_content: NULL terinated string to write to the file
  *
  * Return: 1, on Success. -1 on failure(file can not be created, file can not
- * 	   be written, write "fails", etc , ...)
+ * be written, write "fails", etc , ...)
  * AUTHOR: BASIL BASSEY
  */
 int create_file(const char *filename, char *text_content)
