749de9ef4b0e324e115ada7ba287a5cdfc7d32e8
Basil Bassey <akpanbasilbassey@gmail.com>
2023-04-09 12:50:35 -0700

    Making corrections

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,10 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 
 int _putchar(char c);
@@ -14,4 +12,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* _MAIN_H */
+#endif /* MAIN_H */
