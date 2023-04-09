34c6f64ba5dfe2e3245214a8017a81b1d07c699a
Basil Bassey <akpanbasilbassey@gmail.com>
2023-04-09 12:28:04 -0700

    Adding Task 1 filee

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * read_textfiles - a function that reads a text file and prints it to the
+ *                  POSIX standard output.
+ * @filename: file pathname.
+ * @letter: number of letters to read or print
+ * 
+ * Returns: actual number of letter it could read and print
+ * AUTHOR: BASIL BASSEY
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    ssize_t file, wr, rd;
+    char *buff;
+    if (filename == NULL)
+        return 0;
+    
+    buff = malloc(sizeof(char) * letters);
+    if (buff == NULL)
+        return 0;
+    file = open(filename, O_RDONLY);
+    rd = read(STDOUT_FILENO, buff, letters);
+    wr = write(file, buff, letters);
+    if (file == -1 || wr == -1 || rd == -1)
+    {
+        free(buff);
+        return 0;
+    }
+
+    free(buff);
+    return wr;
+}
+
