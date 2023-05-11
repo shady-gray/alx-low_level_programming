81e42afbea6779c911cca4669d9f09b50a1b67cd
Basil Bassey <akpanbasilbassey@gmail.com>
2023-05-10 20:20:27 -0700

    Making correction to Task 0 file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,27 +11,29 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, wr, rd;
+	int file;
+	ssize_t wr, rd;
 	char *buff;
 
 	if (filename == NULL)
 		return (0);
-	buff = malloc(letters * sizeof(char));
-	if (buff == NULL)
-		return (0);
 	file = open(filename, O_RDONLY);
 	if (file == -1)
+		return (0);
+	buff = malloc(letters * sizeof(char));
+	if (buff == NULL)
 	{
-		free(buff);
+		close(file);
 		return (0);
 	}
 	rd = read(file, buff, letters);
+	close(file);
 	if (rd == -1)
 	{
 		free(buff);
 		return (0);
 	}
-	wr = write(STDOUT_FILENO, buff, letters);
+	wr = write(STDOUT_FILENO, buff, rd);
 
 	if (wr != rd || wr == -1)
 	{
