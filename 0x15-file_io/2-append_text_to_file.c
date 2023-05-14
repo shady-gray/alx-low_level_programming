bca8c99d2f6da2105c8bd003aeb11a2e770f75a8
Basil Bassey <akpanbasilbassey@gmail.com>
2023-05-14 12:35:46 -0700

    Adding task 2 file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* append_text_to_file - a function that appends text at the end of a file.
+* @filename: file (path)name
+* @text_content: text to be appended
+*
+* Return: 1 on success and -1 on failure
+* Author: BASIL BASSEY
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, txtlen;
+	ssize_t wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_APPEND | O_WRONLY);
+	if (!file || file == -1)
+		return (-1);
+
+	for (txtlen = 0; text_content[txtlen]; txtlen++)
+		;
+	if (text_content != NULL)
+	wr = write(filename, text_content, txtlen)
+	close(file);
+	if (wr == -1)
+		return (-1);
+	return (1);
+}
