06bf81b72089235aa71cf3b350b5f5b984de7d3e
Basil Bassey <akpanbasilbassey@gmail.com>
2023-05-14 13:01:18 -0700

    Correcting Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	for (txtlen = 0; text_content[txtlen]; txtlen++)
 		;
 	if (text_content != NULL)
-	wr = write(file, text_content, txtlen)
+	wr = write(file, text_content, txtlen);
 	close(file);
 	if (wr == -1)
 		return (-1);
