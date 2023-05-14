9f184e3f9c536bc020012dbc7ba2626f029a9570
Basil Bassey <akpanbasilbassey@gmail.com>
2023-05-14 14:09:58 -0700

    Correcting task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,23 +1,5 @@
 #include "main.h"
 
-size_t _strlen(char *str);
-int create_file(const char *filename, char *text_content);
-
-/**
- * _strlen - counts length of a string
- * @str: string to be counted
- *
- * Return: length of string
- * Author: BASIL BASSEY
- */
-size_t _strlen(char *str)
-{
-	size_t  len;
-
-	for (len = 0; *str != '\0'; len++)
-		str++;
-	return (len);
-}
 
 /**
  * create_file - a function that creates a file
@@ -35,10 +17,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	strlen = _strlen(text_content);
-	file = open(filename, O_WRONLY | O_CREAT, 0600);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	for (strlen = 0; text_content[strlen]; strlen++)
+		;
 	wr = write(file, text_content, strlen);
 	if (wr == -1)
 	{
@@ -46,6 +31,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	close(file);
-	return(1);
+	return (1);
 }
 
