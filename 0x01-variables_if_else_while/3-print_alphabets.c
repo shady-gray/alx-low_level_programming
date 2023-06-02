f845972c463fc7039b36b592e18de85d8c0135c8
Basil Bassey <akpanbasilbassey@gmail.com>
2023-06-02 05:17:27 -0700

    Adding Task 3 file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints the alphabet in lowercase then in uppercase, followed by a new line.
+ * main - prints the alphabet in lowercase then in uppercase,
+ * followed by a new line.
  *
  * Return: 0 (on success)
 */
@@ -10,7 +11,7 @@ int main(void)
 
 	for (c = 97; c < 123; c++)
 		putchar(c);
-    for (c = 65; c < 91; c++)
+	for (c = 65; c < 91; c++)
 		putchar(c);
 	putchar(10);
 	return (0);
