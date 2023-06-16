4bd5055f2984736a04265831f7ae1cb864820e98
Basil Bassey <akpanbasilbassey@gmail.com>
2023-06-17 00:48:22 +0100

    Adding Task 0

---
+++ 0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - prints elements in list
+ * @h: begunning of linked list
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t m = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		m++;
+	}
+	return (m);
+}
