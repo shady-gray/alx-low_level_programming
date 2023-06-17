b198643c1bc0f06bcd1464b871ace456253e5a7d
Basil Bassey <akpanbasilbassey@gmail.com>
2023-06-17 01:04:31 +0100

    Adding Task 3

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add new node to front of list
+ *
+ * @head: head of the list
+ * @n: value of the element
+ * Return: Address of new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *h;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	h = *head;
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
+	new->next = h;
+	if (h != NULL)
+		h->prev = new;
+	*head = new;
+	return (new);
+}
