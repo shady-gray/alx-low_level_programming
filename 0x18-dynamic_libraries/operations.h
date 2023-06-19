0c58d760e9af30f114f1f68d7617dee08c39ef86
Basil Bassey <akpanbasilbassey@gmail.com>
2023-06-19 20:18:23 +0100

    Header file for task 2

---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,10 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+signed int add(signed int a, signed int b);
+signed int sub(signed int a, signed int b);
+signed int mul(signed int a, signed int b);
+signed int div(signed int a, signed int b);
+signed int mod(signed int a, signed int b);
+
+#endif /*ifndef OPERATIONS_H*/
